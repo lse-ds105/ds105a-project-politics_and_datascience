9f23df21d185ad6e55737e7a74d61871690e61ae
MaddoxLeigh <m.leigh@lse.ac.uk>
2023-12-07 16:36:26 +0000

    website lookin good now

--- stream.py
+++ stream.py
@@ -26,7 +26,7 @@ def createpopulationgeo():
     us_population_df = pd.read_csv("docs/Population_And_Median_Income")
     states_geo = alt.topo_feature(data.us_10m.url, 'states')
     chart = alt.Chart(states_geo).mark_geoshape().encode(
-        color= alt.Color('Population:Q',scale=alt.Scale(scheme='greensst'), title='Population'),
+        color= alt.Color('Population:Q',scale=alt.Scale(scheme='greens'), title='Population'),
         tooltip=['State:N', 'Population:Q']
     ).transform_lookup(
         lookup='id',
@@ -57,18 +57,72 @@ def createpopulationdensitygeo():
         title='Population Density by State'
     )
     return chart
+def createpopagainstincome():
+    merged_population_income = pd.read_csv("docs/Population_And_Median_Income")
+    merged_population_income['Income_Integer'] = merged_population_income['Median Household income'].replace('[\$,]', '', regex=True).astype(int)
+    merged_population_income['Population/SqMi'] = pd.to_numeric(merged_population_income['Population/SqMi'], errors='coerce')
+    scatter = alt.Chart(merged_population_income).mark_circle().encode(
+    y= alt.Y('Income_Integer',title="Median Household Income"),
+    x='Population/SqMi',
+    tooltip=['State:N', 'Income_Integer:Q', 'Population/SqMi:Q']
+    )
+    regressionline = scatter.transform_regression('Income_Integer', 'Population/SqMi').mark_line()
+    combined_chart = (scatter + regressionline)
+    return combined_chart.interactive()
+
+
+
+def createcrimeratesbetweenUKcities():
+    crimeratesdf = pd.read_csv("crimerates2021.csv")
+    Crimegraph = alt.Chart(crimeratesdf).mark_line().encode(
+    x=alt.X('Month:O', title='Month'),  
+    color=alt.Color('city:N', title='City'), 
+    y=alt.Y('crime_rate:Q', title='Crime Rates'),
+    tooltip=['Month:O', 'city:N', 'crime_rate:Q'] 
+    ).properties(
+        width=500, 
+        height=300,
+        title='Crime Rates in Cities (2021)'
+    ).transform_fold(  
+        fold=['London', 'Manchester', 'Newcastle', 'Portsmouth' ],
+        as_=['city', 'crime_rate']
+    )
+    return Crimegraph
+    
+def createlondoncrimebycategory():
+    crimecategorylondon2022 = pd.read_csv("crimecategorylondon2022.csv")
+    chart = alt.Chart(crimecategorylondon2022).mark_bar().encode(
+    x='count:Q',
+    y=alt.Y('category:N', sort='-x'),
+    tooltip=['category:N', 'count:Q'] 
+    ).properties(
+    title='Crime by Category in London 2021'  # Add a title to the chart
+    )
+    return chart
+
+
 
 st.title('Political Data Science project')
-st.write("Group Members: Alex Gabriella-Faith || Ayse Yalcin || Maddox Leigh")
-st.header("Population by state")
-st.altair_chart(createpopulationbar())
-st.header("Population vs Population Density by State")
+st.markdown("* Alex Faith (alexgabriellafaith) | BSc in Politics and Data Science")
+st.markdown("* Ayşe Yalçın (ayseyalcin1) | BSc in Politics and Data Science")
+st.markdown("* Maddox Leigh (maddoxleigh) | BSc in Politics and Data Science")
+st.write("This project aims to create easy to interpret, interactive graphs showing interesting correlations we found from various government census data and APIs")
+st.write("It is important to note that any correlations shown do not implicitly imply that a causation exists between the two variables!")
+st.markdown("# US data")
+st.markdown("## Population vs Population Density by State")
 col1, col2 = st.columns(2)
 with col1:
     st.altair_chart(createpopulationgeo())
 
 with col2:
     st.altair_chart(createpopulationdensitygeo())
+st.markdown("## Median Household income (2019-2021) against population density")
+st.altair_chart(createpopagainstincome())
+st.markdown("# UK data")
+st.markdown("## Crime rates over 2021 between UK cities")
+st.altair_chart(createcrimeratesbetweenUKcities())
+st.markdown("## Crime by category, London 2022")
+st.altair_chart(createlondoncrimebycategory())
 
     
 
