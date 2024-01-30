9ec29ddfbc72824786332bd0cdb717b8b23a6ca1
MaddoxLeigh <m.leigh@lse.ac.uk>
2024-01-30 11:33:03 +0000

    second chart added to site

--- docs/webstream.py
+++ docs/webstream.py
@@ -3,10 +3,11 @@ import pandas as pd
 import streamlit as st
 import altair as alt
 from vega_datasets import data
+
 def createpopulationgeo():
     from vega_datasets import data
     from sqlalchemy import create_engine
-    engine = create_engine('sqlite:///PoliticsandDataSci.db', echo=False, isolation_level="AUTOCOMMIT")
+    engine = create_engine('sqlite:///Data/PoliticsandDataSci.db', echo=False, isolation_level="AUTOCOMMIT")
     with engine.connect() as conn:
         pass
     us_population_df = pd.read_sql_table("State Data",con=engine)
@@ -25,22 +26,30 @@ def createpopulationgeo():
         title='Population by State'
     )
     return chart
-def createpopulationdf():
+
+def createpopulationdensitygeo():
     from vega_datasets import data
-    us_population_df = pd.read_csv("Population_And_Median_Income")
+    from sqlalchemy import create_engine
+    engine = create_engine('sqlite:///Data/PoliticsandDataSci.db', echo=False, isolation_level="AUTOCOMMIT")
+    with engine.connect() as conn:
+        pass
+    us_population_df = pd.read_sql_table("State Data",con=engine)
     states_geo = alt.topo_feature(data.us_10m.url, 'states')
     chart = alt.Chart(states_geo).mark_geoshape().encode(
-        color= alt.Color('Population:Q',scale=alt.Scale(scheme='greens'), title='Population'),
-        tooltip=['State:N', 'Population:Q']
+        color=alt.Color('Population/SqMi:Q',scale=alt.Scale(scheme='blues'), title='Population/Sq/Mi'),
+        tooltip=['State:N', 'Population/SqMi:Q']
     ).transform_lookup(
         lookup='id',
-        from_=alt.LookupData(us_population_df, 'id', ['Population','State'])
+        from_=alt.LookupData(us_population_df, 'id', ['Population/SqMi','State'])
     ).project(
         type='albersUsa'
     ).properties(
         width=500,
         height=300,
-        title='Population by State'
+        title='Population Density by State'
     )
-    return us_population_df.head()
+    return chart
+
+
 st.altair_chart(createpopulationgeo())
+st.altair_chart(createpopulationdensitygeo())
