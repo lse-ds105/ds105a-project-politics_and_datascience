3c0927eeef867774c6b48a7ab4ff89547cf40742
MaddoxLeigh <m.leigh@lse.ac.uk>
2024-01-30 12:07:37 +0000

    edit website

--- docs/webstream.py
+++ docs/webstream.py
@@ -49,7 +49,31 @@ def createpopulationdensitygeo():
         title='Population Density by State'
     )
     return chart
+st.title('Political Data Science project')
+st.markdown("* Alex Faith (alexgabriellafaith) | BSc in Politics and Data Science")
+st.markdown("* Ayşe Yalçın (ayseyalcin1) | BSc in Politics and Data Science")
+st.markdown("* Maddox Leigh (maddoxleigh) | BSc in Politics and Data Science")
+st.write("This project aims to create easy to interpret, interactive graphs showing interesting correlations we found from various government census data and APIs")
+st.write("It is important to note that any correlations shown do not implicitly imply that a causation exists between the two variables!")
+st.markdown("# US data")
+st.markdown("## Population vs Population Density by State")
+col1, col2 = st.columns(2)
+with col1:
+    st.altair_chart(createpopulationgeo())
+
+with col2:
+    st.altair_chart(createpopulationdensitygeo())
+
+#st.markdown("## Median Household income (2019-2021) against population density")
+#st.altair_chart(createpopagainstincome())
+#st.markdown("# UK data")
+#st.markdown("## Crime rates over 2021 between UK cities")
+#st.altair_chart(createcrimeratesbetweenUKcities())
+#st.markdown("## Crime by category, London 2022")
+#st.altair_chart(createlondoncrimebycategory())
 
 
-st.altair_chart(createpopulationgeo())
-st.altair_chart(createpopulationdensitygeo())
+
+
+#st.altair_chart(createpopulationgeo())
+#st.altair_chart(createpopulationdensitygeo())
